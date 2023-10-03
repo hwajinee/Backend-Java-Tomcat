8618aa66e72d5133a940133c98ae6e0d6ccda010
최화진 <hvvxzin99@gmail.com>
2023-10-03 16:55:10 +0900

    요구사항 1 : index.html 반환

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,27 +1,38 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import db.Repository;
+import model.User;
+import http.util.IOUtils;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static http.util.HttpRequestUtils.*;
 
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
     private static final String ROOT_URL = "./webapp";
     private static final String HOME_URL = "/index.html";
-//    private static final String ROOT_URL = "./webapp";
-//    private static final String ROOT_URL = "./webapp";
-//    private static final String ROOT_URL = "./webapp";
+    private static final String LOGIN_FAILED_URL = "/user/login_failed.html";
+    private static final String LOGIN_URL = "/user/login.html";
+    private static final String USER_LIST_URL = "/user/list.html";
 
+
+    private final Repository repository;
     private final Path homePath = Paths.get(ROOT_URL + HOME_URL);
+
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
+        repository = MemoryUserRepository.getInstance();
     }
 
     @Override
@@ -35,41 +46,54 @@ public class RequestHandler implements Runnable{
 
             // 시작줄 분석
             String startLine = br.readLine();
-            String[] startLines = startLine.split(" ");
-            String method = startLines[0];
-            String url = startLines[1];
+            String[] startLineElements = startLine.split(" ");
+            String method = startLineElements[0];
+            String url = startLineElements[1];
+            System.out.printf("httpMethod : %s\nurl : %s\n", method, url);
 
-            // header info
             int requestContentLength = 0;
             String cookie = "";
 
-            while(true) {
+            while (true) {
                 final String line = br.readLine();
                 if (line.equals("")) {
                     break;
                 }
-
-                if(line.startsWith("Content-Length")) {
-                    requestContentLength = Integer.parseInt(line.split(":")[1]);
+                // header info
+                if (line.startsWith("Content-Length")) {
+                    requestContentLength = Integer.parseInt(line.split(": ")[1]);
                 }
-                if(line.startsWith("cookie")) {
-                    cookie = line.split(":")[1];
+
+                if (line.startsWith("Cookie")) {
+                    cookie = line.split(": ")[1];
                 }
             }
 
-            //요구사항 1번
-            if (method.equals("GET") && url.endsWith(".html")) {
+            // 요구 사항 1번
+            if (method.equals("GET") && (url.endsWith("/index.html") || url.endsWith("/"))) {
                 body = Files.readAllBytes(Paths.get(ROOT_URL + url));
             }
-            if (url.equals("/")) {
-                body = Files.readAllBytes(homePath);
-            }
 
+
+
+
+
+            response200Header(dos, body.length);
+            responseBody(dos, body);
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
     }
+
+    private void login(DataOutputStream dos, Map<String, String> queryParameter, User user) {
+        if (user != null && user.getPassword().equals(queryParameter.get("password"))) {
+            response302HeaderWithCookie(dos,HOME_URL);
+            return;
+        }
+        response302Header(dos,LOGIN_FAILED_URL);
+    }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
@@ -77,6 +101,42 @@ public class RequestHandler implements Runnable{
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response200HeaderWithCss(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response302HeaderWithCookie(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("Set-Cookie: logined=true" + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
